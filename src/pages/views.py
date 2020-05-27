6b7084ad28c5e9962e085bea69a84fb2516c6341
hagios <hagioswilson@gmail.com>
2020-05-27 08:13:38 +0100

    did more on loops, conditions and template tags

--- src/pages/views.py
+++ src/pages/views.py
@@ -15,4 +15,13 @@ def contact_view(request, *args, **kwargs):
 
 def about_view(request, *args, **kwargs):
     # return HttpResponse('<h1>Hello World</h1>')
-    return render(request, 'about.html')
+
+    my_context = {
+
+        "info": "this is about our company ....",
+        "Num_workers": 2000,
+        "mylist" : ['Joshua', 'Prince', 'Claudy', 'Gidi'],
+        "numbs": [100, 125, 300, 2000, 500, 5000, 6000]
+    }
+
+    return render(request, 'about.html', my_context)
