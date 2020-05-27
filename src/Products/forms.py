be3608d9c90cd51784cc3f0833be3c0ec9122927
hagios <hagioswilson@gmail.com>
2020-05-27 12:52:02 +0100

    form tweeks and form validation

--- src/Products/forms.py
+++ src/Products/forms.py
@@ -1,3 +1,45 @@
 from django import forms
 from .models import Product
 
+class ProductForm(forms.ModelForm):
+    #customizing form fields and front end validation
+
+    #recommended practice use same names as field name in db
+
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "custom placeholder"}))
+    description = forms.CharField(
+
+        required = False,
+        widget =forms.Textarea(attrs={
+            "placeholder":"Your description",
+            "class": "form-control",
+            "id":"desc",
+            "rows":10,
+            "cols": 25
+
+        })
+    )
+
+    price = forms.DecimalField()
+
+    # class Meta:
+    #     model = Product
+    #     fields = ['title', 'description', 'price']
+    
+    def clean_title(self, *args, **kwargs):
+
+        title = self.cleaned_data.get('title')
+
+        if len(title) < 8:
+            raise forms.ValidationError("This has shorter chars")
+        else:
+            return title
+         
+
+class RawProductForm(forms.Form):
+
+    pass
+
+#     title       = forms.CharField()
+#     description = forms.CharField();
+#     price       = forms.DecimalField()
--- src/Products/views.py
+++ src/Products/views.py
@@ -2,16 +2,58 @@ from django.shortcuts import render
 
 from Products.models import Product
 
+from Products.forms import ProductForm, RawProductForm
+
 # Create your views here.
 def view_product_detail(request):
 
     obj = Product.objects.get(id=1)
 
-    context = {
-
-        'title': obj.title,
-        'description': obj.description,
-        'summary': obj.summary
-    }
+    context = {'object' : obj}
 
     return render(request, 'products/details.html', context)
+
+
+def view_product_form(request):
+ 
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid():
+
+
+        # Product.objects.create(**form.cleaned_data)
+
+        form.save()
+
+        form = ProductForm()
+
+    context = {'form' : form}
+
+    return render(request, 'products/product_form.html', context)
+
+
+# def view_product_form(request):
+
+     
+#     form = RawProductForm()
+
+#     if request.method == 'POST':
+
+#         form = RawProductForm(request.POST)
+
+#         if form.is_valid():
+
+#             print(form.cleaned_data)
+
+#             Product.objects.create(**form.cleaned_data)
+        
+#         else:
+
+#             print(form.errors)
+
+
+    # context = {'form' : form}
+
+    # return render(request, 'products/product_form.html', context)
+
+    
