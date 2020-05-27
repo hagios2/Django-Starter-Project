c5239a9915bd68534346bbabe1bda3d1fd53bedc
hagios <hagioswilson@gmail.com>
2020-05-27 09:40:21 +0100

    overriding templates

--- src/Products/views.py
+++ src/Products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from Products.models import Product
+
 # Create your views here.
+def view_product_detail(request):
+
+    obj = Product.objects.get(id=1)
+
+    context = {
+
+        'title': obj.title,
+        'description': obj.description,
+        'summary': obj.summary
+    }
+
+    return render(request, 'products/details.html', context)
